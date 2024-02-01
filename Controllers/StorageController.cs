256f6511e2105b1dfeee1a49d177214879914ff5
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2024-02-01 14:29:30 +0500

    Add project files.

---
+++ Controllers/StorageController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyStorage.Services;
+
+namespace MyStorage.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class StorageController : ControllerBase
+    {
+        private readonly StorageServices _storageServices;
+        public StorageController(StorageServices storageServices)
+        {
+            _storageServices = storageServices;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(IFormFile file, CancellationToken cancellationToken = default)
+        {
+            return Ok(await _storageServices.Save(file, cancellationToken));
+        }
+
+        [HttpGet("{fileName}")]
+        public IActionResult Get([FromRoute] string fileName)
+        {
+            var (filePath, contentType) = _storageServices.Get(fileName);
+            return PhysicalFile(filePath, contentType);
+        }
+    }
+}
---
+++ Services/StorageServices.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.OpenApi.Any;
+
+namespace MyStorage.Services
+{
+    public class StorageServices
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly FileExtensionContentTypeProvider _contentTypeProvider;
+        public StorageServices(IWebHostEnvironment env)
+        {
+            _env = env;
+            _contentTypeProvider = new FileExtensionContentTypeProvider();
+        }
+
+        public async Task<Uri> Save(IFormFile file, CancellationToken cancellationToken)
+        {
+            string webRootPath = _env.WebRootPath;
+            string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            string filePath = Path.Combine(webRootPath, fileName);
+
+            using var fileStream = new FileStream(filePath, FileMode.Create);
+            await file.CopyToAsync(fileStream, cancellationToken);
+
+            var fullUrl = "https://storage.sarvarbekabduqodirov.uz:8443/" + "api/" + fileName;
+            return new Uri(fullUrl);
+        }
+
+        public (string, string) Get(string fileName)
+        {
+            string webRootPath = _env.WebRootPath;
+            var filePath = Path.Combine(webRootPath, fileName);
+            if (_contentTypeProvider.TryGetContentType(fileName, out var contentType))
+            {
+                return (filePath, contentType);
+            }
+
+            return (filePath, "application/octet-stream");
+        }
+    }
+}
